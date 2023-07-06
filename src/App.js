fde390b472072f525b870c64d71f94af706b2b29
Semih Gervan <semihgervan@hotmail.com>
2023-07-06 11:53:02 +0300

    S10G3 Projesi

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
+import { useDispatch, useSelector } from "react-redux";
+import { addList, nextMovie, prevMovie, removeList } from "./actions";
 
 function App() {
-  const [sira, setSira] = useState(0);
-  const favMovies = [];
+  const favMovies = useSelector((store) => store.favMovies);
+  const movies = useSelector((store) => store.movies)
+  const sira = useSelector((store) => store.sira);
+  const dispatch = useDispatch()
 
-  function sonrakiFilm() {
-    setSira(sira + 1);
+
+  function handleAddList() {
+    dispatch(addList());
   }
+
+  function handleNextMovie() {
+    dispatch(nextMovie())
+  }
+
+  function handlePrevMovie() {
+    dispatch(prevMovie())
+  }
+
+  function handleRemoveList() {
+    dispatch(removeList(movies[sira].id))
+  }
+
 
   return (
     <div className="wrapper max-w-2xl mx-auto">
@@ -23,18 +41,31 @@ function App() {
       </nav>
       <Switch>
         <Route exact path="/">
+          {favMovies.includes(movies)}
           <Movie sira={sira} />
 
           <div className="flex gap-3 justify-end py-3">
             <button
-              onClick={sonrakiFilm}
+              onClick={handlePrevMovie}
+              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+            >
+              Önceki
+            </button>
+
+            <button
+              onClick={handleNextMovie}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Sıradaki
             </button>
-            <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
-              Listeme ekle
-            </button>
+            {!favMovies.includes(movies[sira]) ?
+              <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white" onClick={handleAddList}>
+                Listeme ekle
+              </button>
+              :
+              <button className="select-none px-4 py-2 bg-red-700 hover:bg-red-600 text-white" onClick={handleRemoveList}>
+                Listemden Çıkar
+              </button>}
           </div>
         </Route>
 
